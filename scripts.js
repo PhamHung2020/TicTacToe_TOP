90b14daf154003515d44e43e65e891dbc868183f
phammanhhung <phammanhhung10501@gmail.com>
2021-03-02 10:59:54 +0700

    Add medium and hard level

--- scripts.js
+++ scripts.js
@@ -102,7 +102,9 @@ const gameBoardView = (function()
         buttons.forEach(button => button.disabled = isDisable);
     }
 
-    return { init, setPos, getPos, checkWin, clear, disable };
+    function getBoard() { return board; };
+
+    return { init, setPos, getPos, checkWin, clear, disable, getBoard };
 })();
 
 const choice_section = (function() {
@@ -169,24 +171,41 @@ const choice_section = (function() {
 const computer = (
     function computerMove()
     {
-        function easyMove(gameBoardView)
+        function easyMove(board)
         {
             let pos = 0;
-            while (gameBoardView.getPos(pos) != '')
+            while (board[pos] != '')
             {
                 pos = Math.floor(Math.random() * 9);
             }
             return pos;
         }
 
-        function mediumMove()
+        function mediumMove(board, count, playerName, computerName)
         {
-
+            if (count < 4) return easyMove(board);
+            else return hardMove(board, count, playerName, computerName);
         }
 
-        function hardMove()
+        function hardMove(board, count, playerName, computerName)
         {
-
+            let bestScore = -2;
+            let bestMove = -1;
+            for (let i = 0; i < 9; ++i)
+            {
+                if (board[i] == '') 
+                {
+                    board[i] = computerName;
+                    let eval = AI.minimax(board, count + 1, false, computerName, playerName);
+                    board[i] = '';
+                    if (bestScore < eval[0])
+                    {
+                        bestScore = eval[0];
+                        bestMove = i;
+                    }
+                }
+            }
+            return bestMove;
         }
 
         return { easyMove, mediumMove, hardMove };
@@ -218,7 +237,10 @@ const game = (
             choice_section.disable(true);
             gameBoardView.disable(false);
             if (computerName == 'X')
-                computerMove();    
+            {
+                computerMove();
+                ++count;
+            }  
             announce.textContent = 'Playing';
         }
 
@@ -239,6 +261,8 @@ const game = (
                 return;
             }
             ++count;
+            if (count == 9)
+                end(null);
             computerMove();
             if (gameBoardView.checkWin())
             {
@@ -246,7 +270,7 @@ const game = (
                 return;
             }
             ++count;
-            if (count >= 8)
+            if (count == 9)
                 end(null);
         }
 
@@ -255,13 +279,16 @@ const game = (
             switch (level)
             {
                 case 'easy':
-                    gameBoardView.setPos(computer.easyMove(gameBoardView), computerName);
+                    gameBoardView.setPos(computer.easyMove(gameBoardView.getBoard()), computerName);
                     break;
                 case 'medium':
-                    gameBoardView.setPos(computer.mediumMove(), computerName);
+                    let mediumMove = computer.mediumMove(gameBoardView.getBoard(), count + 1, playerName, computerName);
+                    gameBoardView.setPos(mediumMove, computerName);
                     break;
-                case 'hard': gameBoardView.setPos(computer.hardMove(), computerName);
-                break;
+                case 'hard': 
+                    let hardMove = computer.hardMove(gameBoardView.getBoard(), count + 1, playerName, computerName);
+                    gameBoardView.setPos(hardMove, computerName);
+                    break;  
             }
         }
 
@@ -270,7 +297,7 @@ const game = (
             if (winnerName != null)
                 announce.textContent = 'The winner is ' + winnerName;
             else
-                announce.textContent = 'Draw!!!';
+                announce.textContent = 'Tie!!!';
             gameBoardView.disable(true);
         }
         
@@ -284,6 +311,71 @@ const game = (
         return {start, restart, subscribe};
     }
 )();
+
+const AI = {
+    checkLine : function(index1, index2, index3, board)
+    {
+        if (board[index1] == board[index2] && 
+            board[index2] == board[index3] && 
+            board[index1] == board[index3] &&
+            board[index1] != '')
+            return board[index1];
+        return null;
+    },
+
+    checkWin : function(board)
+    {
+        return (this.checkLine(0, 1, 2, board) || this.checkLine(3, 4, 5, board) || 
+                this.checkLine(6, 7, 8, board) || this.checkLine(0, 3, 6, board) || 
+                this.checkLine(1, 4, 7, board) || this.checkLine(2, 5, 8, board) ||
+                this.checkLine(0, 4, 8, board) || this.checkLine(2, 4, 6, board));
+    },
+
+    minimax(board, count, isMaximize, maximize, minimize)
+    {
+        let checkWinResult = this.checkWin(board);
+        if (checkWinResult == maximize) return [1, count];
+        else if (checkWinResult == minimize) return [-1, count];
+        if (count == 9) return [0, 9];
+
+        if (isMaximize)
+        {
+            let res = [-2, count];
+            for (let i = 0; i < 9; ++i)
+            {
+                if (board[i] == '')
+                {
+                    board[i] = maximize;
+                    let eval = this.minimax(board, count + 1, false, maximize, minimize);
+                    board[i] = '';
+                    if (res[0] < eval[0])
+                    {
+                        res = eval;
+                    }
+                }
+            }
+            return res;
+        }
+        else
+        {
+            let res = [2, count];
+            for (let i = 0; i < 9; ++i)
+            {
+                if (board[i] == '')
+                {
+                    board[i] = minimize;
+                    let eval = this.minimax(board, count + 1, true, maximize, minimize);
+                    if (res[0] > eval[0])
+                    {
+                        res = eval;
+                    }
+                    board[i] = '';
+                }
+            }
+            return res;
+        }
+    }
+}
 
 gameBoardView.init();
 choice_section.init();
