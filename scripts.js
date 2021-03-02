53cb61f82d5bebb4dfd4bab35db347049f783c96
phammanhhung <phammanhhung10501@gmail.com>
2021-03-02 17:23:03 +0700

    Refactor code

--- scripts.js
+++ scripts.js
@@ -1,46 +1,55 @@
 const pubSub = (
-    function()
-    {
+    function () {
         const events = {};
-        function subscribe(eventName, callbackFunc)
-        {
+
+        function subscribe(eventName, callbackFunc) {
             if (!events[eventName])
                 events[eventName] = [];
             events[eventName].push(callbackFunc);
         }
-        function publish(eventName, data)
-        {
+
+        function publish(eventName, data) {
             if (!events[eventName]) return;
             events[eventName].forEach(callbackFunc => callbackFunc(data));
         }
 
-        return { subscribe, publish };
+        return { subscribe, publish }
     }
 )();
 
+function gameBoard() {
+    const board = ['', '', '', '', '', '', '', '', ''];
 
-const Level = {
-    easy : "easy",
-    medium : "medium",
-    hard : "hard"
+    function getBoard(index) { return board[index]; }
+
+    function setBoard(index, char) {
+        if (index >= 0 && index <= 8) board[index] = char; 
+    }
+
+    function checkLine(index1, index2, index3) {
+        if (board[index1] == board[index2] && board[index2] == board[index3] &&
+            board[index1] == board[index3] && board[index1] != '')
+            return board[index1];
+        return null;
+    }
+
+    function checkWin() {
+        return (checkLine(0, 1, 2, board) || checkLine(3, 4, 5, board) ||
+                checkLine(6, 7, 8, board) || checkLine(0, 3, 6, board) ||
+                checkLine(1, 4, 7, board) || checkLine(2, 5, 8, board) ||
+                checkLine(0, 4, 8, board) || checkLine(2, 4, 6, board));
+    }
+
+    return { getBoard, setBoard, checkWin }
 }
 
-// function player(name)
-// {
-//     const playerName = name;
-//     const getName = () => playerName;
-//     return { getName };
-// }
-
-const gameBoardView = (function() 
-{
+const gameBoardView = (function () {
     const boardView = document.getElementById('game-board');
     const buttons = boardView.childNodes;
-    const board = ['', '', '', '', '', '', '', '', ''];
-    function init()
-    {
-        for (let i = 0; i < 9; ++i)
-        {
+    const board = gameBoard();
+
+    function init() {
+        for (let i = 0; i < 9; ++i) {
             const newButton = document.createElement('button');
             newButton.id = `button-${i}`;
             boardView.appendChild(newButton);
@@ -49,8 +58,7 @@ const gameBoardView = (function()
         disable(true);
     }
 
-    function bindEvent()
-    {
+    function bindEvent() {
         buttons.forEach(button => {
             button.addEventListener('click', (e) => {
                 const index = +e.target.id[e.target.id.length - 1];
@@ -59,176 +67,111 @@ const gameBoardView = (function()
         });
     }
 
-    function getPos(index)
-    {
-        return board[index];
-    }
-
-    function setPos(index, playerName)
-    {
-        if (playerName == 'X' || playerName == 'O')
-            board[index] = playerName;
+    function setPos(index, playerName) {
+        board.setBoard(index, playerName);
         buttons[index].textContent = playerName;
     }
 
-    function checkLine(index1, index2, index3)
-    {
-        if (board[index1] == board[index2] && 
-            board[index2] == board[index3] && 
-            board[index1] == board[index3] &&
-            board[index1] != '')
-            return true;
-        return false;
-    }
-
-    function checkWin()
-    {
-        if (checkLine(0, 1, 2) || checkLine(3, 4, 5) || checkLine(6, 7, 8) ||
-            checkLine(0, 3, 6) || checkLine(1, 4, 7) || checkLine(2, 5, 8) ||
-            checkLine(0, 4, 8) || checkLine(2, 4, 6))
-            return true;
-        return false;
-    }
-
-    function clear()
-    {
+    function clear() {
         buttons.forEach(button => button.textContent = '');
-        for (let i = 0; i < 9; ++i) board[i] = '';
-        count = 0;
+        for (let i = 0; i < 9; ++i) board.setBoard(i, '');
     }
 
-    function disable(isDisable)
-    {
+    function disable(isDisable) {
         buttons.forEach(button => button.disabled = isDisable);
     }
 
-    function getBoard() { return board; };
+    function getBoard() { return board; }
 
-    return { init, setPos, getPos, checkWin, clear, disable, getBoard };
+    return { init, setPos, clear, disable, getBoard }
 })();
 
-const choice_section = (function() {
+const choice_section = (function () {
     const buttonX = document.getElementById('x');
     const buttonO = document.getElementById('o');
     const buttonStart = document.getElementById('start-restart');
     const selectLevel = document.getElementById('level');
 
-    function init()
-    {
+    function init() {
         buttonX.classList.add('border');
         bindEvent();
     }
 
-    function disable(isDisable)
-    {
+    function disable(isDisable) {
         selectLevel.disabled = isDisable;
         buttonX.disabled = isDisable;
         buttonO.disabled = isDisable;
     }
 
-    function bindEvent()
-    {
-        buttonO.addEventListener('click', function() { switchWeapon(buttonO) });
-        buttonX.addEventListener('click', function() { switchWeapon(buttonX) });
+    function bindEvent() {
+        buttonO.addEventListener('click', function () { switchWeapon(buttonO) });
+        buttonX.addEventListener('click', function () { switchWeapon(buttonX) });
         buttonStart.addEventListener('click', () => {
-            if (buttonStart.textContent == 'Start')
-            {
+            if (buttonStart.textContent == 'Start') {
                 buttonStart.textContent = 'Restart';
                 pubSub.publish('start', null);
             }
-            else
-            {
+            else {
                 buttonStart.textContent = 'Start';
                 pubSub.publish('restart', null);
             }
         });
     }
 
-    function switchWeapon(buttonClicked)
-    {
-        if (!buttonClicked.classList.contains('border'))
-        {
+    function switchWeapon(buttonClicked) {
+        if (!buttonClicked.classList.contains('border')) {
             buttonX.classList.toggle('border');
             buttonO.classList.toggle('border');
         }
     }
 
-    function getPlayerWeapon()
-    {
-        if (buttonX.classList.contains('border'))
-            return 'X';
+    function getPlayerWeapon() {
+        if (buttonX.classList.contains('border')) return 'X';
         return 'O';
     }
 
-    function getLevel()
-    {
-        return selectLevel.value;
-    }
+    function getLevel() { return selectLevel.value; }
 
-    return {init, disable, getPlayerWeapon, getLevel};
+    return { init, disable, getPlayerWeapon, getLevel }
 })();
 
 const computer = (
-    function computerMove()
-    {
-        function easyMove(board)
-        {
+    function computerMove() {
+        function easyMove(board) {
             let pos = 0;
-            while (board[pos] != '')
-            {
+            while (board.getBoard(pos) != '') {
                 pos = Math.floor(Math.random() * 9);
             }
             return pos;
         }
 
-        function mediumMove(board, count, playerName, computerName)
-        {
-            if (count < 4) return easyMove(board);
+        function mediumMove(board, count, playerName, computerName) {
+            if (count < 3) return easyMove(board);
             else return hardMove(board, count, playerName, computerName);
         }
 
-        function hardMove(board, count, playerName, computerName)
-        {
-            let bestScore = -2;
-            let bestMove = -1;
-            for (let i = 0; i < 9; ++i)
-            {
-                if (board[i] == '') 
-                {
-                    board[i] = computerName;
-                    let eval = AI.minimax(board, count + 1, false, computerName, playerName);
-                    board[i] = '';
-                    if (bestScore < eval[0])
-                    {
-                        bestScore = eval[0];
-                        bestMove = i;
-                    }
-                }
-            }
-            return bestMove;
+        function hardMove(board, count, playerName, computerName) {
+            return minimax(board, count, true, computerName, playerName)[2];
         }
 
-        return { easyMove, mediumMove, hardMove };
+        return { easyMove, mediumMove, hardMove }
     }
 )();
 
 const game = (
-    function()
-    {
+    function () {
         let playerName = null;
         let computerName = null;
         let level = null;
         let count = 0;
         const announce = document.getElementById('announce-winner');
-        function start()
-        {
-            if (choice_section.getPlayerWeapon() == 'X')
-            {
+
+        function start() {
+            if (choice_section.getPlayerWeapon() == 'X') {
                 playerName = 'X';
                 computerName = 'O';
             }
-            else
-            {
+            else {
                 playerName = 'O';
                 computerName = 'X';
             }
@@ -237,144 +180,84 @@ const game = (
             choice_section.disable(true);
             gameBoardView.disable(false);
             if (computerName == 'X')
-            {
-                computerMove();
-                ++count;
-            }  
+                nextTurn(computerMove(), computerName);
             announce.textContent = 'Playing';
         }
 
-        function restart()
-        {
+        function restart() {
             choice_section.disable(false);
             gameBoardView.clear();
             gameBoardView.disable(true);
             announce.textContent = 'Press start to play';
         }
 
-        function move(index)
-        {
-            gameBoardView.setPos(index, playerName);
-            if (gameBoardView.checkWin())
-            {
+        function move(index) {
+            if (!nextTurn(index, playerName)) nextTurn(computerMove(), computerName);
+        }
+
+        function nextTurn(index, char) {
+            gameBoardView.setPos(index, char);
+            if (gameBoardView.getBoard().checkWin()) {
                 end(playerName);
-                return;
+                return true;
             }
             ++count;
-            if (count == 9)
+            if (count == 9) {
                 end(null);
-            computerMove();
-            if (gameBoardView.checkWin())
-            {
-                end(computerName);
-                return;
+                return true;
             }
-            ++count;
-            if (count == 9)
-                end(null);
+            return false;
         }
 
-        function computerMove()
-        {
-            switch (level)
-            {
+        function computerMove() {
+            switch (level) {
                 case 'easy':
-                    gameBoardView.setPos(computer.easyMove(gameBoardView.getBoard()), computerName);
-                    break;
+                    return computer.easyMove(gameBoardView.getBoard());
                 case 'medium':
-                    let mediumMove = computer.mediumMove(gameBoardView.getBoard(), count + 1, playerName, computerName);
-                    gameBoardView.setPos(mediumMove, computerName);
-                    break;
-                case 'hard': 
-                    let hardMove = computer.hardMove(gameBoardView.getBoard(), count + 1, playerName, computerName);
-                    gameBoardView.setPos(hardMove, computerName);
-                    break;  
+                    return computer.mediumMove(gameBoardView.getBoard(), count, playerName, computerName);
+                case 'hard':
+                    return computer.hardMove(gameBoardView.getBoard(), count, playerName, computerName);
             }
         }
 
-        function end(winnerName)
-        {
+        function end(winnerName) {
             if (winnerName != null)
                 announce.textContent = 'The winner is ' + winnerName;
             else
                 announce.textContent = 'Tie!!!';
             gameBoardView.disable(true);
         }
-        
-        function subscribe()
-        {
-            pubSub.subscribe('start', function() { start(); });
-            pubSub.subscribe('restart', function() { restart(); });
-            pubSub.subscribe('move', function(index) { move(index); });
+
+        function subscribe() {
+            pubSub.subscribe('start', function () { start(); });
+            pubSub.subscribe('restart', function () { restart(); });
+            pubSub.subscribe('move', function (index) { move(index); });
         }
 
-        return {start, restart, subscribe};
+        return { start, restart, subscribe }
     }
 )();
 
-const AI = {
-    checkLine : function(index1, index2, index3, board)
-    {
-        if (board[index1] == board[index2] && 
-            board[index2] == board[index3] && 
-            board[index1] == board[index3] &&
-            board[index1] != '')
-            return board[index1];
-        return null;
-    },
+function minimax(board, count, isMaximize, playerCurrent, playerNext)
+{
+    let checkWinResult = board.checkWin();
+    if (checkWinResult == playerCurrent)
+        return [isMaximize ? 1 : -1, count, -1];
+    else if (checkWinResult == playerNext)
+        return [isMaximize ? -1 : 1, count, -1];
+    if (count == 9) return [0, 9, null];
 
-    checkWin : function(board)
-    {
-        return (this.checkLine(0, 1, 2, board) || this.checkLine(3, 4, 5, board) || 
-                this.checkLine(6, 7, 8, board) || this.checkLine(0, 3, 6, board) || 
-                this.checkLine(1, 4, 7, board) || this.checkLine(2, 5, 8, board) ||
-                this.checkLine(0, 4, 8, board) || this.checkLine(2, 4, 6, board));
-    },
-
-    minimax(board, count, isMaximize, maximize, minimize)
-    {
-        let checkWinResult = this.checkWin(board);
-        if (checkWinResult == maximize) return [1, count];
-        else if (checkWinResult == minimize) return [-1, count];
-        if (count == 9) return [0, 9];
-
-        if (isMaximize)
-        {
-            let res = [-2, count];
-            for (let i = 0; i < 9; ++i)
-            {
-                if (board[i] == '')
-                {
-                    board[i] = maximize;
-                    let eval = this.minimax(board, count + 1, false, maximize, minimize);
-                    board[i] = '';
-                    if (res[0] < eval[0])
-                    {
-                        res = eval;
-                    }
-                }
-            }
-            return res;
-        }
-        else
-        {
-            let res = [2, count];
-            for (let i = 0; i < 9; ++i)
-            {
-                if (board[i] == '')
-                {
-                    board[i] = minimize;
-                    let eval = this.minimax(board, count + 1, true, maximize, minimize);
-                    if (res[0] > eval[0])
-                    {
-                        res = eval;
-                    }
-                    board[i] = '';
-                }
-            }
-            return res;
+    let res = [isMaximize ? -2 : 2, count, -1];
+    for (let i = 0; i < 9; ++i) {
+        if (board.getBoard(i) == '') {
+            board.setBoard(i, playerCurrent);
+            const eval = this.minimax(board, count + 1, !isMaximize, playerNext, playerCurrent);
+            board.setBoard(i, '');
+            if (isMaximize == (res[0] < eval[0]))
+                res = [eval[0], eval[1], i];
         }
     }
+    return res;
 }
 
 gameBoardView.init();
